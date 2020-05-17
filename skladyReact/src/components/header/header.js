973b94061a10300cae0ae44e05a37e2fa86af377
Maria <maria@wzyk.io>
2020-05-17 11:18:47 +0200

    dziala

--- skladyReact/src/components/header/header.js
+++ skladyReact/src/components/header/header.js
@@ -2,12 +2,12 @@ import React from "react";
 import { RiArrowLeftSLine } from "react-icons/ri";
 import "./header.css";
 
-const Header = () => {
+const Header = ({ handleopenLeading }) => {
   return (
     <div className="header">
       <div id="tytul1">SKŁADY</div>
       
-    <RiArrowLeftSLine id="arrow"/>
+    <RiArrowLeftSLine id="arrow" onClick={handleopenLeading}/>
       
     </div>
   );

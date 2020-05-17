40bad96fde657908d4d6c41afa0960842aa79dab
Maria <maria@wzyk.io>
2020-05-17 09:59:25 +0200

    leading + popr kolor backgroundu app

---
+++ skladyReact/src/components/Leading/Leading.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Leading.css";
+
+const Leading = () => {
+    return (
+        <div id="leadingbox">
+        <div id="contentbox">
+            <h1>SKŁADY</h1>
+            <p>Twoja świadoma pielęgnacja</p>
+            <button>Sprawdź skład</button>
+            <button>Lista składników</button>
+            <button>O aplikacji</button>
+        </div>
+        </div>
+    )
+}
+export default Leading;

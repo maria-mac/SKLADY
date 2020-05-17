cc8cba48d5b9ab79b8cfd57a871035a6229a7aef
Maria <maria@wzyk.io>
2020-05-17 13:13:45 +0200

    dziala otwieranie i zamykaniie

--- skladyReact/src/components/Landing/Landing.js
+++ skladyReact/src/components/Landing/Landing.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Landing.css";
 import Lista from "../Lista/Lista";
 
@@ -6,16 +6,24 @@ const Landing = ({ handleCloseLanding, show }) => {
 
     const showHide = show ? "modal display-block" : "modal display-none";
 
+    const [showLista, setShowLista] = useState(false);
+    const openLista = () => {
+        setShowLista(true)
+    };
+    const closeLista = () => {
+        setShowLista(false)
+    };
+
     return (
         <div id="landingbox" className={showHide}>
         <div id="contentbox">
             <div id="tytul">SKŁADY</div>
             <div id="podtytul">Twoja świadoma pielęgnacja</div>
             <button className="btn" onClick={handleCloseLanding}>Sprawdź skład</button>
-            <button className="btn">Lista składników</button>
+            <button className="btn" onClick={openLista}>Lista składników</button>
             <button className="btn">O aplikacji</button>
         </div>
-        <Lista />
+        <Lista show={showLista} handleCloseLista={closeLista}/>
         </div>
     )
 }
--- skladyReact/src/components/Lista/Lista.js
+++ skladyReact/src/components/Lista/Lista.js
@@ -2,10 +2,13 @@ import React from "react";
 import "./Lista.css";
 import { RiArrowLeftSLine } from "react-icons/ri";
 
-const Lista = () => {
+const Lista = ({ show, handleCloseLista }) => {
+
+    const showHide = show ? "modal display-block" : "modal display-none";
+
     return (
-        <div id="listabox">
-<RiArrowLeftSLine id="arrow1" />
+        <div id="listabox" className={showHide}>
+<RiArrowLeftSLine id="arrow1" onClick={handleCloseLista}/>
 <div id="listatext">Lista</div>
         </div>
     )
